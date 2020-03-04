2ca2e660fb7d22171aeb7adc420e34279076c3c1
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-03-04 14:06:02 -0800

    storyboards layed out, needs hooking up

---
+++ 5thWheelRV/5thWheelRV/RVOwnerListingsTableViewController.swift
@@ -0,0 +1,45 @@
+//
+//  RVOwnerListingsTableViewController.swift
+//  5thWheelRV
+//
+//  Created by Jorge Alvarez on 3/4/20.
+//  Copyright © 2020 Jorge Alvarez. All rights reserved.
+//
+
+import UIKit
+
+// SearchListingSegue
+// EditReservationSegue
+// SearchResultsCell (other tvc)
+class RVOwnerListingsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ReservationCell", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+    }
+
+}
