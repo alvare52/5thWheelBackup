d41ad27bf02f5c3953c5670789d2d5e299dd8342
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-03-06 02:56:24 -0800

    2 UI tests done, tests are giving errors due to project name starting with the number 5

--- 5thWheelRV/WheelRVUITests/WheelRVUITests.swift
+++ 5thWheelRV/WheelRVUITests/WheelRVUITests.swift
@@ -7,37 +7,39 @@
 //
 
 import XCTest
+@testable import _thWheelRV
 
 class WheelRVUITests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-
+        let app = XCUIApplication()
+        app.launch()
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        // In UI tests it’s important to set the initial state -
+        // such as interface orientation - required for your tests before they run.
+        //The setUp method is a good place to do this.
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    func testChangeLoginType() {
 
-    func testExample() {
-        // UI tests must launch the application that they test.
         let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        //let loginVC = LoginViewController()
+    app/*@START_MENU_TOKEN@*/.buttons["Log In"]/*[[".segmentedControls.buttons[\"Log In\"]",".buttons[\"Log In\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        //app/*@START_MENU_TOKEN@*/.buttons["Sign Up"]/*[[".segmentedControls.buttons[\"Sign Up\"]",".buttons[\"Sign Up\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        //XCTAssertEquals(loginVC.loginType, LoginType.logIn)
+        //XCTAssertTrue(loginVC.loginType == .logIn)
     }
 
-    func testLaunchPerformance() {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
+    func testChangeUserType() {
+
+        let app = XCUIApplication()
+        //let loginVC = LoginViewController()
+        app.buttons["RV Owner"].tap()
+        //app/*@START_MENU_TOKEN@*/.buttons["Sign Up"]/*[[".segmentedControls.buttons[\"Sign Up\"]",".buttons[\"Sign Up\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        //XCTAssertEquals(loginVC.loginType, LoginType.logIn)
+        //XCTAssertTrue(loginVC.loginType == .logIn)
     }
 }
